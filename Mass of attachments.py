4f9c1b16661d551a98340c71ab0064085283a56e
raphc1 <raphmethner.dhc8@gmail.com>
2022-12-13 15:00:21 +0100

    bending stress bottom

--- Mass of attachments.py
+++ Mass of attachments.py
@@ -1,6 +1,8 @@
+from math import pi
 def stress_shear_force_curtain():
     return
 
-def bending_stress_bottom_curtain():
+def bending_stress_bottom_curtain(diameter, thickness, lateral_load, height):
+    I_xx = (pi * thickness * diameter ** 3)/8
 
-    return
+    return (lateral_load * height * diameter)/(2 * I_xx)
