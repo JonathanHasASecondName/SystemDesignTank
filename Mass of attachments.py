41b6eefd844f5f13be9d1807a8122848a44e512e
ThomasClaudel <thomas.claudel@icloud.com>
2022-12-13 15:41:19 +0100

    update

--- Mass of attachments.py
+++ Mass of attachments.py
@@ -1,2 +1,11 @@
-def stress_shear_force_curtain():
-    return
+import numpy as np
+import math
+mass_tank = 0
+
+def stress_shear_force_curtain(mass_tank,thickness_curtain,radius_curtain):
+    V_y = (2*9.81*mass_tank)/2
+    I_x = (math.pi * thickness_curtain * (radius_curtain/2)**3)/8
+    Q_x = 4 * radius_curtain**2 * thickness_curtain
+    shear_stress = (V_y*Q_x)/(I_x*thickness_curtain)
+    return shear_stress
+
