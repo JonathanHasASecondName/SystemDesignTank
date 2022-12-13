ade12e7d785c405d0eef48b96c81de7a2e38ad9d
jonathan <jonathanpoot3009@gmail.com>
2022-12-13 14:16:31 +0100

    added buckling stress

--- buckling.py
+++ buckling.py
@@ -12,7 +12,7 @@ def find_stress_column_buckling(A,L,I,E):
 
 def find_stress_shell_buckling(p, E, R, t_1, labda, v, L):
     Q = p/E * (R/t_1)**2
-    k = labda + 12/np.pi**4 * L**4/(R**2*t_1**2) * (1-v**2)/labda
+    k = labda + (12/np.pi**4) * (L**4/(R**2*t_1**2)) * ((1-v**2)/labda)
 
     part_a = 1.983 - 0.983*np.exp(-23.14*Q)
     part_b = k*np.pi**2 * E/(12*(1-v**2))
