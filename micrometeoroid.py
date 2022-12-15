beb704286bab2fd786bcc2eb2a1410166552b4c6
hihihellohihi3998 <mateo.arganza@gmail.com>
2022-12-15 13:53:40 +0100

    micrometeoroid updated

--- micrometeoroid.py
+++ micrometeoroid.py
@@ -1,7 +1,7 @@
-
+import numpy as np
 
 #INPUT TO FUNCTION IN SI UNITS
-def find_penetration_depth(mass_p,velocity_p,density_micrometeoroid):
+def find_penetration_depth_1layer(mass_p,velocity_p,density_micrometeoroid):
     t=0.7*((mass_p*10**3)**0.352)*((velocity_p*10**-3)**0.875)*((density_micrometeoroid*10**-3)**0.167)
     return t
 
@@ -15,4 +15,33 @@ def find_penetration_depth(mass_p,velocity_p,density_micrometeoroid):
 #WORST CASE MicroMeteorioid ----> m=10^-4 g , 17km/s (MASS?) (DENSITY?) WORST CASE NICKEL 8900 kg/m^3
 #https://www.science.org/doi/10.1126/science.262.5133.550
 
-print(find_penetration_depth(10**-7,17000,8900))
+#print(find_penetration_depth_1layer(mass_p=2,velocity_p=8000,density_micrometeoroid=2700))
+
+
+def find_bumper_thickness(projectile_diameter,bumper_density,density_micrometeoroid):
+    t_bumper=0.25*projectile_diameter*density_micrometeoroid/bumper_density
+    return t_bumper
+def find_penetration_depth_2layer(mass_p,
+                                  velocity_p,
+                                  density_micrometeoroid,
+                                  bumper_density,
+                                  spacing,
+                                  yield_stress):
+    # FROM SI UNITS TO FUNKY FORMULA UNITS (SEE SOURCE)
+    mass_p=mass_p*0.001
+    velocity_p = velocity_p*0.001
+    density_micrometeoroid=density_micrometeoroid*0.001
+    bumper_density=bumper_density*0.001
+    spacing=spacing*100
+    yield_stress=yield_stress*1.4038e-7
+    print(yield_stress)
+
+    t_rear_wall=0.055*(density_micrometeoroid*bumper_density)**(1/6)*mass_p**(1/3)*velocity_p/np.sqrt(spacing)*np.sqrt(70/yield_stress)
+    return t_rear_wall
+
+#print(find_penetration_depth_2layer(mass_p=1,
+#                                 velocity_p=1,
+#                                 density_micrometeoroid=1,
+#                                 bumper_density=1,
+#                                 spacing=1,
+#                                 yield_stress=1))
