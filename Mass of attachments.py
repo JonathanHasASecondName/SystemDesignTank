9699ab85f306ff44c6d5656121007c2c7250b037
ThomasClaudel <thomas.claudel@icloud.com>
2022-12-15 14:53:56 +0100

    update

--- Mass of attachments.py
+++ Mass of attachments.py
@@ -1,32 +1,61 @@
 import numpy as np
 import math
 mass_tank = 0
-material
-def stress_shear_crosssection(mass_tank,thickness_curtain,radius_curtain):
-    V_y = (2*9.81*mass_tank)/2
-    I_x = (math.pi * thickness_curtain * (radius_curtain/2)**3)/8
-    Q_x = 2*radius_curtain**2 * thickness_curtain
-    shear_stress = (V_y*Q_x)/(I_x*thickness_curtain)
+material_poisson_ratio =
+material_shear_stress =
+material_axial_stress =
+flat_distance = 0.04
+bottom_margin = 0.1
+height_curtain = #needs to be found
+radius_curtain =
+materials =
+
+#[MaterialName,density,material_axial_stress,material_axial_stress,material_shear_stress]
+
+def stress_shear_crosssection(thickness_curtain,radius_curtain):
+    V = (1.5*9.81*mass_tank)/2
+    I = (math.pi * thickness_curtain * (radius_curtain/2)**3)/8
+    Q = 2*radius_curtain**2 * thickness_curtain
+    shear_stress = (V*Q)/(I*thickness_curtain)
     return shear_stress
 
-def bending_stress_bottom_curtain(diameter, thickness, lateral_load, height, mass_tank):
-    lateral_load = 2 * 9.81 * mass_tank
-    I_xx = (math.pi * thickness * diameter ** 3)/8
-    bending_stress = (0.5 * lateral_load * height * diameter)/(2 * I_xx)
-
-
-
+def bending_stress_curtain(radius_curtain,thickness_curtain,height_curtain):
+    lateral_load = 1.5 * 9.81 * mass_tank
+    I = math.pi * thickness_curtain*(radius_curtain)**3
+    bending_stress = (lateral_load * height_curtain * radius_curtain)/(2 * I)
     return bending_stress
 
+def shear_stress_connection(radius_curtain):
+    V = 4 * 9.81 * mass_tank
+    I = 2*math.pi*radius_curtain*(flat_distance**2)
+    Q = ((flat_distance**2)*radius_curtain*math.pi)/2
+    t = 2*math.pi*radius_curtain
+    shear_stress = (V*Q)/(I*t)
+    return shear_stress
 
-def shear_stress_connection():
-    return
+def axial_stresses(thickness_curtain,radius_curtain):
+    F= 6*9.81*mass_tank2
+    A= thickness_curtain*2*math.pi*radius_curtain
+    axial_stress = F/A
+    return axial_stress
 
-def axial_stresses(mass_tank,thickness_curtain,radius_curtain,height_curtain):
-    F_lateral = (2*9.81*mass_tank)/2
-    F_axial = 6*9.81*mass_tank
-    A_lateral = height_curtain*thickness_curtain*2
-    A_axial = thickness_curtain*2*math.pi*radius_curtain
-    axial_stress_lateral = F_lateral/A_lateral
-    axial_stress_axial = F_axial/A_axial
-    return max(axial_stress_lateral,axial_stress_axial)
+def shell_buckling(thickness_curtain,radius_curtain,height_curtain,poisson_ratio):
+    Buckles = False
+    #get buckling stress
+    if buckling_stress > maxial_stresses(thickness_curtain,radius_curtain):
+        Buckles = True
+    return Buckles
+
+def configuration_loop(height_curtain):
+    for material in materials:
+        for thickness_curtain in range(0,0.1,0.001):
+            if (shell_buckling(thickness_curtain,radius_curtain,height_curtain,poisson_ratio) == False) and (axial_stresses(thickness_curtain,radius_curtain)<= material_axial_stress) and (bending_stress_curtain(radius_curtain,thickness_curtain,height_curtain)<= material_axial_stress) and (shear_stress_connection(radius_curtain)<= material_shear_stress) and (stress_shear_crosssection(thickness_curtain,radius_curtain) <= material_shear_stress):
+                print(thickness_curtain, material)
+                break
+
+print('First')
+configuration_loop()
+
+
+
+
